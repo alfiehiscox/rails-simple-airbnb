edeeedc3345b22cc14dd9b11633428e88b59e125
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-11 16:37:15 +0000

    add actions to flats_controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,26 @@
 class FlatsController < ApplicationController
+  # read
+  def index
+  end
+
+  def show
+  end
+
+  # create
+  def new
+  end
+
+  def create
+  end
+
+  # update
+  def update
+  end
+
+  def edit
+  end
+
+  # Delete
+  def destroy
+  end
 end
