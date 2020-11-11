8201c9ef843f7df81d56c5bb7e961fb7d6192032
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-11 16:44:31 +0000

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying Database"
+Flat.destroy_all
+
+6.times do
+  puts "Creating Flat"
+  params = {
+      name: Faker::Address.street_name,
+      address: Faker::Address.street_address,
+      description: Faker::Quote.famous_last_words,
+      price_per_night: rand(50..150),
+      number_of_guests: rand(1..10)
+  }
+  flat = Flat.new(params)
+  flat.save
+end
